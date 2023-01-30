20919b7490149461f35e7b8ce911c8596158d161
joris <fse@gmail.com>
2023-01-30 14:43:45 +0100

    finished house

--- Week1/001.House/src/House.java
+++ Week1/001.House/src/House.java
@@ -5,6 +5,8 @@ import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
+import java.awt.geom.Line2D;
+
 public class House extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -17,6 +19,26 @@ public class House extends Application {
 
 
     public void draw(FXGraphics2D graphics) {
+// Omtrek
+        graphics.draw(new Line2D.Double(500, 500, 500 ,300));
+        graphics.draw(new Line2D.Double(500, 300, 650 ,100));
+        graphics.draw(new Line2D.Double(650, 100, 800 ,300));
+        graphics.draw(new Line2D.Double(800, 300, 800 ,500));
+        graphics.draw(new Line2D.Double(800, 500, 500 ,500));
+// Deur
+        graphics.draw(new Line2D.Double(550, 500, 550 ,375));
+        graphics.draw(new Line2D.Double(550, 375, 615 ,375));
+        graphics.draw(new Line2D.Double(615, 375, 615 ,500));
+// Raam
+        graphics.draw(new Line2D.Double(650, 425, 650 ,350));
+        graphics.draw(new Line2D.Double(650, 350, 775 ,350));
+        graphics.draw(new Line2D.Double(775, 350, 775 ,425));
+        graphics.draw(new Line2D.Double(775, 425, 650 ,425));
+
+
+
+
+
     }
 
 
