49e7179f57c70f470bb062a72f9ef7dd5f62b303
joris <jr.klein@student.avans.nl>
2023-02-13 12:02:17 +0100

    finished

--- Week1/004.Rainbow/src/Rainbow.java
+++ Week1/004.Rainbow/src/Rainbow.java
@@ -9,18 +9,35 @@ import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
 public class Rainbow extends Application {
+    private Canvas canvas;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
+        this.canvas = new Canvas(640, 480);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(new Group(canvas)));
-        primaryStage.setTitle("Rainbow");
+        primaryStage.setTitle("Hello Java 2D");
         primaryStage.show();
-    }
-    
-    
-    public void draw(FXGraphics2D graphics) {
 
+    }
+
+    public void draw(FXGraphics2D g) {
+        int radiusBinnen = 100;
+        int radiusBuiten = 150;
+        int positieX = 250;
+        int positieY = 250;
+        int hoek = 180;
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+        for(double i = -(Math.PI/180)*hoek; i < 0; i+= 0.001) {
+            x1 = (int)Math.round(radiusBinnen * Math.cos(i)) + positieX;
+            y1 = (int)Math.round(radiusBinnen * Math.sin(i)) + positieY;
+            x2 = (int)Math.round(radiusBuiten * Math.cos(i)) + positieX;
+            y2 = (int)Math.round(radiusBuiten * Math.sin(i)) + positieY;
+            g.setColor(Color.getHSBColor((float)-i/3, 1, 1));
+            g.drawLine(x1, y1, x2, y2);
+        }
     }
     
     
