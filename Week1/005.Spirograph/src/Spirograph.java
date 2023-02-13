09198901306900695bdd259391b3357f800e1e29
joris <jr.klein@student.avans.nl>
2023-02-13 13:05:51 +0100

    finished

--- Week1/005.Spirograph/src/Spirograph.java
+++ Week1/005.Spirograph/src/Spirograph.java
@@ -12,6 +12,7 @@ import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
 public class Spirograph extends Application {
+    private boolean check = true;
     private TextField v1;
     private TextField v2;
     private TextField v3;
@@ -20,22 +21,22 @@ public class Spirograph extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Canvas canvas = new Canvas(1920, 1080);
-       
+
         VBox mainBox = new VBox();
         HBox topBar = new HBox();
         mainBox.getChildren().add(topBar);
         mainBox.getChildren().add(new Group(canvas));
         
-        topBar.getChildren().add(v1 = new TextField("300"));
-        topBar.getChildren().add(v2 = new TextField("1"));
-        topBar.getChildren().add(v3 = new TextField("300"));
+        topBar.getChildren().add(v1 = new TextField("10"));
+        topBar.getChildren().add(v2 = new TextField("10"));
+        topBar.getChildren().add(v3 = new TextField("10"));
         topBar.getChildren().add(v4 = new TextField("10"));
                 
         v1.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
         v2.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
         v3.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
         v4.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
-        
+
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(mainBox));
         primaryStage.setTitle("Spirograph");
@@ -46,18 +47,27 @@ public class Spirograph extends Application {
     public void draw(FXGraphics2D graphics) {
         //you can use Double.parseDouble(v1.getText()) to get a double value from the first textfield
         //feel free to add more textfields or other controls if needed, but beware that swing components might clash in naming
-        double a = 100;
-        double b = 140;
-        double c = 50;
-        double d = 400;
+
+        if (check){
+            graphics.translate(650, 350);
+            check = false;
+        }
+        graphics.setColor(Color.white);
+        graphics.fillRect(-650, -350, 1920, 1920);
+        graphics.setColor(Color.black);
+
+        double a = Double.parseDouble(v1.getText());
+        double b = Double.parseDouble(v2.getText());
+        double c = Double.parseDouble(v3.getText());
+        double d = Double.parseDouble(v4.getText());
         double increment = 0.001;
-        graphics.translate(200, 200);
         for (double i = 0; i < Math.PI; i+= increment) {
             int x = (int) (a * Math.cos(b * i) + c * Math.cos(d * i));
             int y = (int) (a * Math.sin(b * i) + c * Math.sin(d * i));
-            int x2 = (int) (a * Math.cos(b * i-increment) + c * Math.cos(d * i-increment));
-            int y2 = (int) (a * Math.sin(b * i-increment) + c * Math.sin(d * i-increment));
-
+            i+= increment;
+            int x2 = (int) (a * Math.cos(b * i) + c * Math.cos(d * i));
+            int y2 = (int) (a * Math.sin(b * i) + c * Math.sin(d * i));
+            i-= increment;
             graphics.drawLine(x, y, x2, y2);
         }
     }
