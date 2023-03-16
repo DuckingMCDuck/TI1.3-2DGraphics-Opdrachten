995e02fd4fbbb869d443f5a3a89119569c5e329a
joris <jr.klein@student.avans.nl>
2023-03-16 15:34:23 +0100

    finished

--- Week3/001.Rainbow/src/Rainbow.java
+++ Week3/001.Rainbow/src/Rainbow.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.ArrayList;
 
 import javafx.application.Application;
 
@@ -33,6 +34,36 @@ public class Rainbow extends Application {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        Font font = new Font("Arial", Font.PLAIN, 100);
+        AffineTransform tx = new AffineTransform();
+        graphics.translate(200, 200);
+        String word = "regenboog";
+        char letter;
+        ArrayList<Color> colors = new ArrayList<>();
+        colors.add(Color.red);
+        colors.add(Color.ORANGE);
+        colors.add(Color.YELLOW);
+        colors.add(Color.green);
+        colors.add(Color.CYAN);
+        colors.add(Color.BLUE);
+        colors.add(Color.MAGENTA);
+        colors.add(Color.pink);
+        colors.add(Color.RED);
+        graphics.rotate(calculateRad(-90));
+        for (int i = 0; i < word.length(); i++) {
+            Shape shape = font.createGlyphVector(graphics.getFontRenderContext(), (word.charAt(i) + "")).getOutline();
+            graphics.draw(shape);
+            graphics.setColor(colors.get(i));
+            graphics.fill(shape);
+            graphics.setColor(Color.black);
+            graphics.translate(50, 10);
+            graphics.rotate(calculateRad(180/(word.length())));
+        }
+
+    }
+
+    public double calculateRad(int degrees){
+        return degrees/(180/Math.PI);
     }
 
 
