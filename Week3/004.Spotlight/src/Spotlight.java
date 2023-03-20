b7ce1c59fb98c6d016934d2362e1835752361da9
joris <jr.klein@student.avans.nl>
2023-03-20 12:48:27 +0100

    finished.

--- Week3/004.Spotlight/src/Spotlight.java
+++ Week3/004.Spotlight/src/Spotlight.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.awt.geom.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Random;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
@@ -10,6 +11,7 @@ import static javafx.application.Application.launch;
 
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -20,15 +22,20 @@ import org.jfree.fx.ResizableCanvas;
 
 public class Spotlight extends Application {
     private ResizableCanvas canvas;
+    private Stage stage;
+    private double xLocation = 0;
+    private double yLocation = 0;
+
 
     @Override
     public void start(Stage stage) throws Exception
     {
-
+        this.stage = stage;
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
         FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
+        canvas.setOnMouseDragged(e -> mouseDragged(e));
         new AnimationTimer() {
             long last = -1;
 
@@ -49,12 +56,28 @@ public class Spotlight extends Application {
         draw(g2d);
     }
 
+    private void mouseDragged(MouseEvent e) {
+        xLocation = e.getX() - 100;
+        yLocation = e.getY() - 100;
+    }
+
 
     public void draw(FXGraphics2D graphics)
     {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        Shape shape = new Ellipse2D.Double(xLocation, yLocation, 200, 200);
+        graphics.draw(shape);
+        graphics.clip(shape);
+
+
+        Random r = new Random();
+        for(int i = 0; i < 1000; i++) {
+            graphics.setPaint(Color.getHSBColor(r.nextFloat(),1,1));
+            graphics.drawLine((int)(r.nextInt() % stage.getWidth()), (int)(r.nextInt() % stage.getHeight()), (int)(r.nextInt() % stage.getWidth()), (int)(r.nextInt() % stage.getHeight()));
+        }
+        graphics.setClip(null);
     }
 
     public void init()
