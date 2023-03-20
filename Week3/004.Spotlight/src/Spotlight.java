4612ff1c16228ccb0d60cf3965c5f3898f5ef60e
joris <jr.klein@student.avans.nl>
2023-03-20 13:21:35 +0100

    changing background should be still image.

--- Week3/004.Spotlight/src/Spotlight.java
+++ Week3/004.Spotlight/src/Spotlight.java
@@ -3,6 +3,7 @@ import java.awt.geom.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Random;
+import java.util.SplittableRandom;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
@@ -16,6 +17,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import javax.imageio.ImageIO;
+import javax.sound.sampled.Line;
 
 import org.jfree.fx.FXGraphics2D;
 import org.jfree.fx.ResizableCanvas;
@@ -25,6 +27,11 @@ public class Spotlight extends Application {
     private Stage stage;
     private double xLocation = 0;
     private double yLocation = 0;
+    private FXGraphics2D graphics;
+    private enum action {NOTDRAW, DRAWN};
+    private action state = action.DRAWN;
+    private Random r = new Random();
+    private BufferedImage image;
 
 
     @Override
@@ -35,7 +42,8 @@ public class Spotlight extends Application {
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
         FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
-        canvas.setOnMouseDragged(e -> mouseDragged(e));
+        canvas.setOnMouseMoved(e -> mouseMoved(e));
+        canvas.setOnMousePressed(e -> mousePressed(e));
         new AnimationTimer() {
             long last = -1;
 
@@ -56,7 +64,11 @@ public class Spotlight extends Application {
         draw(g2d);
     }
 
-    private void mouseDragged(MouseEvent e) {
+    private void mousePressed(MouseEvent e) {
+        state = action.NOTDRAW;
+    }
+
+    private void mouseMoved(MouseEvent e) {
         xLocation = e.getX() - 100;
         yLocation = e.getY() - 100;
     }
@@ -64,19 +76,23 @@ public class Spotlight extends Application {
 
     public void draw(FXGraphics2D graphics)
     {
+        this.graphics = graphics;
         graphics.setTransform(new AffineTransform());
-        graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        graphics.setBackground(Color.white);
         Shape shape = new Ellipse2D.Double(xLocation, yLocation, 200, 200);
         graphics.draw(shape);
         graphics.clip(shape);
 
+//        if (state == action.NOTDRAW) {
+            Random r = new Random();
+            for (int i = 0; i < 1000; i++) {
+                graphics.setPaint(Color.getHSBColor(r.nextFloat(), 1, 1));
+                graphics.drawLine((int) (r.nextInt() % stage.getWidth()), (int) (r.nextInt() % stage.getHeight()), (int) (r.nextInt() % stage.getWidth()), (int) (r.nextInt() % stage.getHeight()));
+            }
+//            state = action.DRAWN;
+//        }
 
-        Random r = new Random();
-        for(int i = 0; i < 1000; i++) {
-            graphics.setPaint(Color.getHSBColor(r.nextFloat(),1,1));
-            graphics.drawLine((int)(r.nextInt() % stage.getWidth()), (int)(r.nextInt() % stage.getHeight()), (int)(r.nextInt() % stage.getWidth()), (int)(r.nextInt() % stage.getHeight()));
-        }
         graphics.setClip(null);
     }
 
